415154346570c57a8e70f36dc3ec96d045468695
em-egan <140679239+em-egan@users.noreply.github.com>
2023-08-07 21:15:44 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+function changeCursorOnClick() {
+    document.body.style.cursor = `url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/411dbde88354ea071d3dd3fdeb09cc799f77c316/cursor_hand_click.svg'), auto`;
+
+    setTimeout(function() {
+        document.body.style.cursor = originalCursorStyle;
+    }, 1000);
+}
+ 
+window.addEventListener('click', changeCursorOnClick);
+
+
 const uxImage = document.querySelector('.UX-Design');
 const mvImage = document.querySelector('.museums-victoria');
-// const cursorCustom = document.body.style.cursor;
 
 const originalSrcUx = uxImage.src;
 const originalSrcMv = mvImage.src;
-// const originalCursorStyle = cursorCustom.cursor;
+const originalCursorStyle = "url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/f90284fca1ae97f0799d4c12c26e14bfb71a758a/1%20Cursor/cursor_straight.svg', auto)"
 
 const newImageUx = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/59577852c6b4f1faad77bc262253312aa1819d4f/UX_link_hover.svg"
 const newImageSrcMv = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/d9df88206bac8ee9a9fe36f61f45b48e614311e6/2%20Project%20images/MV-mock-up-hover.svg"
-// const newImageCursor = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/411dbde88354ea071d3dd3fdeb09cc799f77c316/cursor_hand_click.svg"
+const newImageCursor = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/411dbde88354ea071d3dd3fdeb09cc799f77c316/cursor_hand_click.svg"
 
 
 function changeImageOnHoverUx() {
@@ -34,16 +44,6 @@ function revertImageOnMouseOutMv() {
 mvImage.addEventListener('mouseover', changeImageOnHoverMv);
 mvImage.addEventListener('mouseout', revertImageOnMouseOutMv);
 
-
-// function changeCursorOnClick() {
-//     document.body.style.cursor = `url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/411dbde88354ea071d3dd3fdeb09cc799f77c316/cursor_hand_click.svg'), auto`;
-
-//     setTimeout(function() {
-//         document.body.style.cursor = originalCursorStyle;
-//     }, 1000);
-// }
- 
-// cursorCustom.addEventListener('click', changeCursorOnClick);
 
 document.querySelector('.hamburger-img').addEventListener('click', function(){
     document.getElementById('menu').classList.add('toggle-open');
