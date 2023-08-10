ca57a08a000398aeabda697d1777852fac44fe5b
em-egan <140679239+em-egan@users.noreply.github.com>
2023-08-10 22:07:19 +1000

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const uxImage = document.querySelector('.UX-Design');
 const originalSrcUx = uxImage.src;
 const originalCursorStyle = "url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/9d4ac453066aac16f6e582cc2341fc67bf440f28/1%20Cursor/cursor_normal.svg'), auto";
 
-const newImageUx = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/59577852c6b4f1faad77bc262253312aa1819d4f/UX_link_hover.svg"
+const newImageUx = "https://raw.githubusercontent.com/em-egan/GA-Final_Project/59dd556263d8966173a5c82028dba3c4e8f13f62/3%20UI%20Elements/buttons/Ux%20Designer%20link/ux-designer-hover.svg"
 
 
 function changeImageOnHoverUx() {
