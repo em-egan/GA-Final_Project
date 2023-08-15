711d477c3025b9e66ff8ed8e567e2b6e96d2b07c
em-egan <140679239+em-egan@users.noreply.github.com>
2023-08-16 08:27:35 +1000

    Update page_one.js

--- page_one.js
+++ page_one.js
@@ -1,4 +1,4 @@
-//Cursor change
+//Cursor change//
 
 function changeCursorOnClick() {
   document.body.style.cursor = `url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/36558a5333a7d172fc0d1b52e1aaca0693fce0a0/1%20Cursor/cursor_clicked.svg'), auto`;
@@ -13,7 +13,7 @@ window.addEventListener('click', changeCursorOnClick);
 const originalCursorStyle = "url('https://raw.githubusercontent.com/em-egan/GA-Final_Project/9d4ac453066aac16f6e582cc2341fc67bf440f28/1%20Cursor/cursor_normal.svg'), auto";
 
 
-//Hamburger menu
+//Hamburger menu//
 
 document.querySelector('.hamburger-img').addEventListener('click', function(){
   document.getElementById('menu').classList.add('toggle-open');
@@ -23,7 +23,7 @@ document.getElementById('menu-close').addEventListener('click', function(){
 })
 
 
-// aside menu selection
+// aside menu selection//
 
 const backgroundSelection = document.querySelector('.background-aside');
 const empathizeSelection = document.querySelector('.empathize-aside');
@@ -31,8 +31,6 @@ const defineSelection = document.querySelector('.define-aside');
 const ideateSelection = document.querySelector('.ideate-aside');
 const prototypeSelection = document.querySelector('.prototype-aside');
 const testSelection = document.querySelector('.test-aside');
-
-
 
 const backgroundHidden = document.querySelector('#interactive-background');
 const empathizeHidden = document.querySelector('#interactive-empathize');
@@ -42,7 +40,7 @@ const prototypeHidden = document.querySelector('#interactive-prototype');
 const testHidden = document.querySelector('#interactive-test');
 
 
-// background hover
+// background hover//
 
 backgroundSelection.addEventListener('mouseover', function handleMouseOver() {
 backgroundHidden.style.visibility = 'visible';
@@ -53,7 +51,7 @@ backgroundHidden.style.visibility = 'hidden';
 });
 
 
-//empathize hover
+//empathize hover//
 
 empathizeSelection.addEventListener('mouseover', function handleMouseOver() {
 empathizeHidden.style.visibility = 'visible';
@@ -64,7 +62,7 @@ empathizeHidden.style.visibility = 'hidden';
 });
 
 
-//define hover
+//define hover//
 
 defineSelection.addEventListener('mouseover', function handleMouseOver() {
 defineHidden.style.visibility = 'visible';
@@ -75,7 +73,7 @@ defineHidden.style.visibility = 'hidden';
 });
 
 
-//ideate hover
+//ideate hover//
 
 ideateSelection.addEventListener('mouseover', function handleMouseOver() {
 ideateHidden.style.visibility = 'visible';
@@ -86,7 +84,7 @@ ideateHidden.style.visibility = 'hidden';
 });
 
 
-//prototype hover
+//prototype hover//
 
 prototypeSelection.addEventListener('mouseover', function handleMouseOver() {
 prototypeHidden.style.visibility = 'visible';
@@ -96,7 +94,7 @@ prototypeSelection.addEventListener('mouseout', function handleMouseOut() {
 prototypeHidden.style.visibility = 'hidden';
 });
 
-//test hover
+//test hover//
 
 testSelection.addEventListener('mouseover', function handleMouseOver() {
 testHidden.style.visibility = 'visible';
@@ -105,6 +103,7 @@ testHidden.style.visibility = 'visible';
 testSelection.addEventListener('mouseout', function handleMouseOut() {
 testHidden.style.visibility = 'hidden';
 });
+
 
 
 
